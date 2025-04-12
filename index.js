3fba41195c458f84f05126f3e852d478ed1b4a93
ratchet34 <maxencefiorina@hotmail.fr>
2025-04-12 17:01:36 +0200

    add more CORS headers

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ app.use(cors({credentials: true, origin: process.env.ORIGIN }));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
 
