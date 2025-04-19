00da690eeb658f2ac7d86d6cc9fb360dd5320414
ratchet34 <maxencefiorina@hotmail.fr>
2025-04-19 19:57:29 +0200

    removed ssl for prod

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const cors = require('cors');
-const session = require('express-session');
-const { client } = require('./controllers/client');
-const { checkAuth } = require('./controllers/users/users');
-const { createUsersRoutes } = require('./routes/users');
-const { createTasksRoutes } = require('./routes/tasks');
-const { createShoppingItemsRoutes, createShoppingIndredientsRoutes, createShoppingLocationsRoutes } = require('./routes/shopping');
+const express = require("express");
+const cors = require("cors");
+const session = require("express-session");
+const { client } = require("./controllers/client");
+const { checkAuth } = require("./controllers/users/users");
+const { createUsersRoutes } = require("./routes/users");
+const { createTasksRoutes } = require("./routes/tasks");
+const {
+  createShoppingItemsRoutes,
+  createShoppingIndredientsRoutes,
+  createShoppingLocationsRoutes,
+} = require("./routes/shopping");
 
 const app = express();
 
@@ -16,19 +20,20 @@ client.connect();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({credentials: true, origin: process.env.ORIGIN }));
+app.use(cors({ credentials: true, origin: process.env.ORIGIN }));
 
-
-app.use(session({
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    secure: process.env.NODE_ENV === 'production', // Ensure secure is set to true for HTTPS
-    sameSite: process.env.NODE_ENV === 'production' ? 'none' : undefined, // Required for cross-origin cookies
-    httpOnly: true, // Prevents JavaScript from accessing the cookie
-   }
-}));
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      secure: process.env.NODE_ENV === "production", // Ensure secure is set to true for HTTPS
+      sameSite: process.env.NODE_ENV === "production" ? "none" : undefined, // Required for cross-origin cookies
+      httpOnly: true, // Prevents JavaScript from accessing the cookie
+    },
+  })
+);
 
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.originalUrl}`);
@@ -43,31 +48,40 @@ createShoppingItemsRoutes(app);
 createShoppingIndredientsRoutes(app);
 createShoppingLocationsRoutes(app);
 
-if (process.env.NODE_ENV === 'production') {
-  const https = require('https');
-  const fs = require('fs');
+if (process.env.NODE_ENV === "production") {
+  // const https = require('https');
+  const fs = require("fs");
 
-  const privateKey  = fs.readFileSync(process.env.PRIVATEKEY, 'utf8');
-  const certificate = fs.readFileSync(process.env.CERTIFICATE, 'utf8');
+  // const privateKey  = fs.readFileSync(process.env.PRIVATEKEY, 'utf8');
+  // const certificate = fs.readFileSync(process.env.CERTIFICATE, 'utf8');
 
-  const credentials = {key: privateKey, cert: certificate};
+  // const credentials = {key: privateKey, cert: certificate};
 
-  app.set('trust proxy', 1);
-  app.use(function(req, res, next) {
+  app.set("trust proxy", 1);
+  app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", process.env.ORIGIN);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
     res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
     next();
   });
 
-  const httpsServer = https.createServer(credentials, app);
+  // const httpsServer = https.createServer(credentials, app);
 
-  httpsServer.listen(3000, () => {
-    console.log('Server running on port 3000');
+  // httpsServer.listen(3000, () => {
+  //   console.log('Server running on port 3000');
+  // });
+  app.listen(3000, () => {
+    console.log("Server running in development mode on port 3000");
   });
 } else {
   app.listen(3000, () => {
-    console.log('Server running in development mode on port 3000');
+    console.log("Server running in development mode on port 3000");
   });
 }
